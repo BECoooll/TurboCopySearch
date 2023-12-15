045dccbb3e5d4d8a8fce7636f871f4b76a0cb80e
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2023-12-15 14:14:43 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from copy_files import *
 from save_paths import  *
 import streamlit as st
+import time
 import wx
 file_types_dict = {
     'Text': ['.txt', '.csv'],
@@ -8,8 +9,9 @@ file_types_dict = {
     'Document': ['.doc', '.docx', '.pdf']}
 
 if __name__ == "__main__":
-    directory_to_search = "F:/CODES/ORCA/ComputerVison/Datasets"
-    destination_folder = 'C:/Users/bolou/Pictures/Dest'
+    start_time = time.time()
+    directory_to_search = "F:/CODES/ORCA/From Github/Fake_Image_Detector/TurboCopySearch/Validation/Real"
+    destination_folder = 'F:/CODES/ORCA/From Github/Fake_Image_Detector/TurboCopySearch/Validation'
     file_type = st.selectbox("What kind of files are you looking for?", ("Image", "Document","Text"))
     file_types={file_type:file_types_dict[file_type]}
     directory_to_search = st.text_input('Path to folder:')
@@ -28,8 +30,7 @@ if __name__ == "__main__":
 
     if st.button('Run Turbo Copy'):
         st.write("Your copy process has started")
-
-        st.write(f"Please be patient, your {file_type} files are being copied to {destination_folder}")
-        st.write(f"Retrieving path of all {file_type} in {destination_folder}")
-        # group_and_save_files(directory_to_search, file_types, txt_file_folder, batch_size)
-        # copy_files_from_text_files(txt_file_folder, destination_folder, max_workers=4)
+        st.write(f"Retrieving path of all {file_type} in the source folder...")
+        group_and_save_files(directory_to_search, file_types, txt_file_folder, batch_size)
+        copy_files_from_text_files(txt_file_folder, destination_folder, max_workers=4)
+        st.write(f"Task finished. All {file_type} have been copied | Total time: {round((time.time() -start_time)/60,2)} minutes ")
